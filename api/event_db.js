147fc2f9251a23c8660a52e2f8d779ba2c41ae09
FireArmor717 <1989164313@qq.com>
2025-10-14 17:14:41 +0800

    debug the error of ts
    reinstall the dependencies

--- api/event_db.js
+++ api/event_db.js
@@ -4,9 +4,9 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: 'localhost',
-  user: 'lwang71_admin',
-  password: 'Wlz@040901',   // <- 如果你的 root 密码不同，改成你的密码
-  database: 'lwang71_MySQL666'  // <- 你之前使用的数据库名
+  user: 'root',
+  password: '666666',   // <- 如果你的 root 密码不同，改成你的密码
+  database: 'MySQL666'  // <- 你之前使用的数据库名
 });
 
 module.exports = pool;
