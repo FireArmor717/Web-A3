867cc9ddb43d4a392633a904fe3051b1d227f1eb
FireArmor717 <184941156+FireArmor717@users.noreply.github.com>
2025-10-15 16:59:18 +0800

    Detail page module complete

--- A3-workspace/projects/client-app/src/app/pages/event-details/event-details.ts
+++ A3-workspace/projects/client-app/src/app/pages/event-details/event-details.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink, Router } from '@angular/router';
+import { ApiService } from '../../services/api';
 
 @Component({
-  selector: 'app-event-details',
-  imports: [],
+  selector: 'page-event-details',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './event-details.html',
-  styleUrl: './event-details.css'
+  styleUrls: ['./event-details.css']
 })
-export class EventDetails {
+export class EventDetails implements OnInit {
+  event: any = null;
+  registrations: any[] = [];
+  loading = true;
+  error = '';
 
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.api.getEvent(id).subscribe({
+      next: (res) => {
+        this.event = res;
+        this.registrations = res.registrations || [];
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Failed to load event details';
+        this.loading = false;
+      }
+    });
+  }
+
+  goHome() {
+  this.router.navigate(['/']);
+  }
+
+  goRegister() {
+    if (!this.event) return;
+    this.router.navigate(['/register', this.event.event_id]);
+  }
 }
