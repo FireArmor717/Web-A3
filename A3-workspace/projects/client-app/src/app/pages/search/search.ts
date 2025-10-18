1da4e542867145809109a9faac1fbc142632a74d
FireArmor717 <184941156+FireArmor717@users.noreply.github.com>
2025-10-18 13:03:50 +0800

    Search page module completed

--- A3-workspace/projects/client-app/src/app/pages/search/search.ts
+++ A3-workspace/projects/client-app/src/app/pages/search/search.ts
@@ -1,11 +1,55 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '../../services/api';
 
 @Component({
-  selector: 'app-search',
-  imports: [],
+  selector: 'page-search',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './search.html',
-  styleUrl: './search.css'
+  styleUrls: ['./search.css']
 })
 export class Search {
+  date = '';
+  location = '';
+  category = '';
+  results: any[] = [];
+  loading = false;
+  message = '';
 
+  constructor(private api: ApiService, private router: Router) {}
+
+  doSearch() {
+    this.loading = true;
+    const params: any = {};
+    if (this.date) params.date = this.date;
+    if (this.location) params.location = this.location;
+    if (this.category) params.category = this.category;
+
+    this.api.searchEvents(params).subscribe({
+      next: (res) => {
+        this.results = res;
+        this.message = res.length ? '' : 'No events found';
+        this.loading = false;
+      },
+      error: () => {
+        this.message = 'Search failed';
+        this.loading = false;
+      }
+    });
+  }
+
+  clearFilters() {
+    this.date = '';
+    this.location = '';
+    this.category = '';
+    this.results = [];
+    this.message = '';
+  }
+
+  viewEvent(id: number) {
+    this.router.navigate(['/event', id]);
+  }
 }
